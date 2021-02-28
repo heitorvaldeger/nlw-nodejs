1f5cbc668d56d904d9478bab3223bad76dbc631f
heitorvaldeger <aurelio.heitor@hotmail.com>
2021-02-27 21:42:58 -0300

    Config nps number

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import { AnswerController } from '../controllers/AnswerController';
+import { NpsController } from '../controllers/NpsController';
+import { SendMailController } from '../controllers/SendMailController';
 import { SurveysController } from '../controllers/SurveysController';
 import { UserController } from '../controllers/UserController';
 const router = Router();
@@ -9,6 +12,15 @@ router.post('/users', userController.create);
 const surveysController = new SurveysController();
 router.get('/surveys', surveysController.findAll);
 router.post('/surveys', surveysController.create);
+
+const sendMailController = new SendMailController();
+router.post('/send-mail', sendMailController.execute);
+
+const answerController = new AnswerController();
+router.get('/answers/:value', answerController.execute);
+
+const npsController = new NpsController();
+router.get('/nps/:survey_id', npsController.execute);
 
 export {
   router
